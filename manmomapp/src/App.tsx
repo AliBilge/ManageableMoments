08d6164cff64789e5a5470146422d14efa6f8352
TrinaT <ttanda@ualberta.ca>
2020-06-04 12:08:51 -0600

    header css changes to match wireframe

--- manmomapp/src/App.tsx
+++ manmomapp/src/App.tsx
@@ -1,30 +1,34 @@
 import *as  React  from 'react';
 import './App.css';
-import { Grid, Container, Header, Icon, Button,Segment } from "semantic-ui-react";
+import { Grid, Container, Icon, Button,Segment } from "semantic-ui-react";
 
 
 function App() {
   return (
-    <div className="App">
-      <Grid.Row columns={2}>
-      <h1 className="ui header">
-      <i className="calendar alternate outline icon"></i>
-      <div className="content"> Manageable Moments </div>
-      </h1>
-      </Grid.Row>
-      <Grid.Row columns={3}>
-        <Container>
-        <button className="ui icon button">
+<div className="App">
+      <div className="ui grid container">
+        <div className="six wide column"></div>
+        <div className="two wide column">
+        <i className="calendar check outline icon massive"></i></div> 
+        <div className="six wide column">
+            <h1> Manageable Moments</h1>
+      <div className="six wide column">,</div>
+  </div>
+
+  <div className="six wide column"></div>
+  <div className="two wide column">
+        <button className="circular orange ui icon button">
             <i className="angle left icon"/>
-            </button >
-            </Container>
-      <Container><h2>June 3, 2020</h2> </Container>
-      <Container>
-      <button className="ui icon button">
-      <i className="angle right icon"/>
+        </button >
+       </div>
+     <div className="four wide column"><h2>June 3, 2020</h2> </div>
+     <div className="two wide column">
+     <button className="circular orange ui icon button">
+      <i className="angle right icon "/>
          </button>
-           </Container>
-      </Grid.Row>
+      </div>
+  
+    </div>
     </div>
   );
 }
