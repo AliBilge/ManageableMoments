6b5aaa3758c3d450bb98ca549a3d96e970967d9b
MSH2020 <mishaydullin@gmail.com>
2020-06-03 21:59:46 -0600

    Started the to do list class

--- my-app/src/components/MyButton/index.js
+++ my-app/src/components/MyButton/index.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
+import './styles.css'
 
 class MyButton extends Component {
     state = {
         buttonValue: 'clickMe'
     }
+    changeButtonValue =()=> {
+        this.setState({
+            buttonValue: "I clicked"
+        }) 
+
+    }
     render() {
         return(
-            <button>
+            <button onClick={this.changeButtonValue}
+            className="changeButton">
                {this.state.buttonValue} 
             </button>
 
