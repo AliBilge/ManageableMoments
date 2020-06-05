debca4ce0835531975a730666eab6e734531d71b
TrinaT <ttanda@ualberta.ca>
2020-06-05 09:42:06 -0600

    changed button placement

--- manmomapp/src/App.tsx
+++ manmomapp/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="ui grid container">
-        <div className="six wide column"></div>
+        <div className="four wide column"></div>
         <div className="two wide column">
           <i className="calendar check outline icon massive"></i></div>
         <div className="six wide column">
@@ -36,37 +36,42 @@ function App() {
         <div className="ui grid container">
           <div className="four wide column"></div>
           <div className="two wide column">
-            <div className="ui mini buttons">
+            <div className="ui tiny buttons">
               <button className="ui orange button">Start</button>
               <div className="or"></div>
               <button className="ui orange button">Pause</button>
             </div>
           </div>
           <div className="one wide column"></div>
-          <div className="two wide column">
+          <div className="three wide column">
             <div className="ui mini buttons">
+              <button className="ui orange button">25<br />minutes</button>
+              <button className="ui orange button">45<br />minutes</button>
+              <button className="ui orange button">90<br />minutes</button>
+            </div>
+          </div>
+          <div className="one wide column"></div>
+          <div className="two wide column">
+            <div className="ui tiny buttons">
               <button className="ui orange button"><i className="volume off icon"></i></button>
               <div className="or"></div>
               <button className="ui orange button"><i className="volume up icon"></i></button>
             </div>
           </div>
-          <div className="two wide column">
-            <div className="ui mini buttons">
-              <button className="ui orange button">25 minutes</button>
-              <button className="ui orange button">45 minutes</button>
-              <button className="ui orange button">90 minutes</button>
-            </div>
-          </div>
+          <div className="ui grid container">
 
+
+
+          </div>
         </div>
       </div>
     </div>
 
-        
-        
-     
-      
-  
+
+
+
+
+
 
   );
 }
