d98ef46d2b8c87934dae183789436870ae0814d8
TrinaT <ttanda@ualberta.ca>
2020-06-05 09:55:17 -0600

    added Priority Task Segment

--- manmomapp/src/App.tsx
+++ manmomapp/src/App.tsx
@@ -1,7 +1,6 @@
-import *as  React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 import { Grid, Container, Icon, Button, Segment } from "semantic-ui-react";
-
 
 function App() {
   return (
@@ -9,7 +8,8 @@ function App() {
       <div className="ui grid container">
         <div className="four wide column"></div>
         <div className="two wide column">
-          <i className="calendar check outline icon massive"></i></div>
+          <i className="calendar check outline icon massive"></i>
+        </div>
         <div className="six wide column">
           <h1> Manageable Moments</h1>
         </div>
@@ -19,9 +19,11 @@ function App() {
         <div className="six wide column"></div>
         <div className="two wide column">
           <button className="circular orange ui icon button">
-            <i className="angle left icon" /> </button >
+            <i className="angle left icon" />{" "}
+          </button>
         </div>
-        <div className="four wide column"><div id="dateToday"></div>
+        <div className="four wide column">
+          <div id="dateToday"></div>
           <div className="two wide column">
             <button className="circular orange ui icon button">
               <i className="angle right icon " />
@@ -45,34 +47,51 @@ function App() {
           <div className="one wide column"></div>
           <div className="three wide column">
             <div className="ui mini buttons">
-              <button className="ui orange button">25<br />minutes</button>
-              <button className="ui orange button">45<br />minutes</button>
-              <button className="ui orange button">90<br />minutes</button>
+              <button className="ui orange button">
+                25
+                <br />
+                minutes
+              </button>
+              <button className="ui orange button">
+                45
+                <br />
+                minutes
+              </button>
+              <button className="ui orange button">
+                90
+                <br />
+                minutes
+              </button>
             </div>
           </div>
           <div className="one wide column"></div>
           <div className="two wide column">
             <div className="ui tiny buttons">
-              <button className="ui orange button"><i className="volume off icon"></i></button>
+              <button className="ui orange button">
+                <i className="volume off icon"></i>
+              </button>
               <div className="or"></div>
-              <button className="ui orange button"><i className="volume up icon"></i></button>
+              <button className="ui orange button">
+                <i className="volume up icon"></i>
+              </button>
             </div>
           </div>
           <div className="ui grid container">
-
-
-
+            <div className="six wide column"></div>
+            <div className="six wide column">
+              <div className="ui raised segments">
+                <div className="ui segment">
+                  <h4>Today's Priority Task</h4>
+                </div>
+                <div className="ui segment">
+                  <p>Finish website </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-
-
-
-
-
-
   );
 }
 
