9b451e4f1790f779a288c9934d3d33a7f8080872
TrinaT <ttanda@ualberta.ca>
2020-06-08 09:03:08 -0600

    changed 4 rows with divs to proper tags

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -6,147 +6,118 @@ import Timer from "react-compound-timer";
 function App() {
   return (
     <div className="App">
-    <div className="ui padded grid container">
-      <div className="five wide column"></div>
-      <div className="two wide column">
-        <i className="calendar check outline icon massive"></i>
+      <div className="ui padded grid container">
+        <div className="five wide column"></div>
+        <div className="two wide column">
+          <i className="calendar check outline icon massive"></i>
+        </div>
+        <div className="six wide column">
+          <h1> Manageable Moments</h1>
+        </div>
+        <div className="six wide column"></div>
       </div>
-      <div className="six wide column">
-        <h1> Manageable Moments</h1>
-      </div>
-      <div className="six wide column"></div>
-    </div>
-    <div className="ui grid container">
-      <div className="six wide column"></div>
-      <div className="two wide column">
-        <button className="circular orange ui icon button">
-          <i className="angle left icon" />
-        </button>
-      </div>
-      <div className="four wide column">
-        <div id="dateToday"></div>
+      <div className="ui grid container">
+        <div className="six wide column"></div>
         <div className="two wide column">
           <button className="circular orange ui icon button">
-            <i className="angle right icon " />
+            <i className="angle left icon" />
           </button>
         </div>
-      </div>
-      <div className="ui grid container">
-        <div className="six wide column"></div>
-        <div className="six wide column">
-        <Timer
-    initialTime={55000}
->
-    {({ start, resume, pause, stop, reset, timerState }) => (
-        <React.Fragment>
-            <div>
-                <Timer.Minutes /> minutes
+        <div className="four wide column">
+          <div id="dateToday"></div>
+          <div className="two wide column">
+            <button className="circular orange ui icon button">
+              <i className="angle right icon " />
+            </button>
+          </div>
+        </div>
+        <Grid container columns={3}>
+          <Grid.Column></Grid.Column>
+          <Grid.Column>
+            <Timer
+              initialTime={1500000}
+              direction="backward"
+            >
+              {({ start, resume, pause, stop, reset, timerState }) => (
+                <React.Fragment>
+                  <div>
+                    <Timer.Minutes /> minutes
                 <Timer.Seconds /> seconds
             </div>
-            <div>{timerState}</div>
-            <br />
-            <div>
-                <button onClick={start}>Start</button>
-                <button onClick={pause}>Pause</button>
-                <button onClick={resume}>Resume</button>
-                <button onClick={stop}>Stop</button>
-                <button onClick={reset}>Reset</button>
-            </div>
-        </React.Fragment>
-    )}
-</Timer>
+                  <div>{timerState}</div>
+                  <br />
+                  <div>
+                    <button className=" ui tiny orange button" onClick={start}>
+                      <i className="play icon"></i>
+                    </button>
+                    <button className=" ui tiny orange button" onClick={pause}>
+                      <i className="pause icon"></i>
+                    </button>
+                    <button className="ui tiny orange button" onClick={stop}>
+                      <i className="stop icon"></i>
+                    </button>
+                    <button className="ui tiny orange button" onClick={reset}>
+                      <i className="repeat icon"></i>
+                    </button>
+                  </div>
+                </React.Fragment>
+              )}
+            </Timer>
 
-          
-        </div>
-        <div className="six wide column"></div>
+
+          </Grid.Column>
+          <Grid.Column></Grid.Column>
+        </Grid>
+
+
       </div>
-      <div className="ui grid container">
-        <div className="four wide column"></div>
-        <div className="one wide column">
-          <div className="ui tiny buttons">
-            <button className="ui orange button">
-            <i className="play icon"></i>
-              </button>
-            <div className="or"></div>
-            <button className="ui orange button">
-              <i className="pause icon"></i></button>
-          </div>
-        </div>
-        <div className="one wide column"></div>
-        <div className="three wide column">
-          <div className="ui mini buttons">
-            <button className="ui orange button">
-              25
-              <br />
-              minutes
-            </button>
-            <button className="ui orange button">
-              45
-              <br />
-              minutes
-            </button>
-            <button className="ui orange button">
-              90
-              <br />
-              minutes
-            </button>
-          </div>
-        </div>
-        <div className="one wide column"></div>
-        <div className="two wide column">
-          <div className="ui tiny buttons">
-            <button className="ui orange button">
-              <i className="volume off icon"></i>
-            </button>
-            <div className="or"></div>
-            <button className="ui orange button">
-              <i className="volume up icon"></i>
-            </button>
-          </div>
-        </div>
-        <div className="ui grid container">
-          <div className="four wide column"></div>
-          <div className="eight wide column">
-            <div className="ui raised segments">
-              <div className="ui segment">
-                <h4>Today's Priority Task</h4>
-              </div>
-              <div className="ui segment">
-                <p>Finish website </p>
-              </div>
+      <Grid container columns={3}>
+        <Grid.Column></Grid.Column>
+        <Grid.Column>
+          <div className="ui raised segments">
+            <div className="ui segment">
+              <h4>Today's Priority Task</h4>
+            </div>
+            <div className="ui segment">
+              <p>Finish website </p>
             </div>
           </div>
-          <div className="ui grid container">
-            <div className="four wide column"></div>
-            <div className="eight wide column">
-              <div className="ui raised segments">
-                <div className="ui segment">
-                  <h4>Additional Tasks</h4>
-                </div>
-                <div className="ui segment">
-                  <p>Link content together </p>
-                </div>
-                <div className="ui segment">
-                  <p>Make it look beautiful </p>
-                </div>
-                <div className="ui button segment">
-                <button className="tiny orange ui button"> Add Task</button>
-                <button className="tiny orange ui button"> Remove Task</button>
-                <button className="tiny orange ui button"> Change Day</button>
-                </div>
-              </div>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid>
+      <Grid container columns={3}>
+        <Grid.Column></Grid.Column>
+        <Grid.Column>
+          <div className="ui raised segments">
+            <div className="ui segment">
+              <h4>Additional Tasks</h4>
+            </div>
+            <div className="ui segment">
+              <p>Link content together </p>
+            </div>
+            <div className="ui segment">
+              <p>Make it look beautiful </p>
+            </div>
+            <div className="ui button segment">
+              <button className="tiny orange ui button"> Add Task</button>
+              <button className="tiny orange ui button"> Remove Task</button>
+              <button className="tiny orange ui button"> Change Day</button>
             </div>
           </div>
-          <div className="ui grid container">
-            <div className="six wide column"></div>
-            <div className="five wide column"><h4>Designed by Code Warriors<br/>2020</h4></div>
-            <div className="six wide column"></div>
-          </div>
-        </div>
-      </div>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid>
+
+
+      <Grid container columns={3}>
+      <Grid.Column></Grid.Column>
+       <Grid.Column><h4>Designed by Code Warriors<br />2020</h4></Grid.Column>
+        <Grid.Column></Grid.Column>
+        </Grid>
     </div>
-    </div>
-);
+
+
+  );
 }
 
 export default App;
