c14fb3131fe87611700fe4fd7b2453a2bde7977f
TrinaT <ttanda@ualberta.ca>
2020-06-08 07:21:03 -0600

    added all code from previous file

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import { Grid, Container, Icon, Button, Segment } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui padded grid container">
+      <div className="five wide column"></div>
+      <div className="two wide column">
+        <i className="calendar check outline icon massive"></i>
+      </div>
+      <div className="six wide column">
+        <h1> Manageable Moments</h1>
+      </div>
+      <div className="six wide column"></div>
     </div>
-  );
+    <div className="ui grid container">
+      <div className="six wide column"></div>
+      <div className="two wide column">
+        <button className="circular orange ui icon button">
+          <i className="angle left icon" />{" "}
+        </button>
+      </div>
+      <div className="four wide column">
+        <div id="dateToday"></div>
+        <div className="two wide column">
+          <button className="circular orange ui icon button">
+            <i className="angle right icon " />
+          </button>
+        </div>
+      </div>
+      <div className="ui grid container">
+        <div className="six wide column"></div>
+        <div className="six wide column"><div className="timerFace"></div><Countdown /></div>
+        <div className="six wide column"></div>
+      </div>
+      <div className="ui grid container">
+        <div className="four wide column"></div>
+        <div className="one wide column">
+          <div className="ui tiny buttons">
+            <button className="ui orange button">
+            <i className="play icon"></i>
+              </button>
+            <div className="or"></div>
+            <button className="ui orange button">
+              <i className="pause icon"></i></button>
+          </div>
+        </div>
+        <div className="one wide column"></div>
+        <div className="three wide column">
+          <div className="ui mini buttons">
+            <button className="ui orange button">
+              25
+              <br />
+              minutes
+            </button>
+            <button className="ui orange button">
+              45
+              <br />
+              minutes
+            </button>
+            <button className="ui orange button">
+              90
+              <br />
+              minutes
+            </button>
+          </div>
+        </div>
+        <div className="one wide column"></div>
+        <div className="two wide column">
+          <div className="ui tiny buttons">
+            <button className="ui orange button">
+              <i className="volume off icon"></i>
+            </button>
+            <div className="or"></div>
+            <button className="ui orange button">
+              <i className="volume up icon"></i>
+            </button>
+          </div>
+        </div>
+        <div className="ui grid container">
+          <div className="four wide column"></div>
+          <div className="eight wide column">
+            <div className="ui raised segments">
+              <div className="ui segment">
+                <h4>Today's Priority Task</h4>
+              </div>
+              <div className="ui segment">
+                <p>Finish website </p>
+              </div>
+            </div>
+          </div>
+          <div className="ui grid container">
+            <div className="four wide column"></div>
+            <div className="eight wide column">
+              <div className="ui raised segments">
+                <div className="ui segment">
+                  <h4>Additional Tasks</h4>
+                </div>
+                <div className="ui segment">
+                  <p>Link content together </p>
+                </div>
+                <div className="ui segment">
+                  <p>Make it look beautiful </p>
+                </div>
+                <div className="ui button segment">
+                <button className="tiny orange ui button"> Add Task</button>
+                <button className="tiny orange ui button"> Remove Task</button>
+                <button className="tiny orange ui button"> Change Day</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="ui grid container">
+            <div className="six wide column"></div>
+            <div className="five wide column"><h4>Designed by Code Warriors<br/>2020</h4></div>
+            <div className="six wide column"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+);
 }
 
 export default App;
